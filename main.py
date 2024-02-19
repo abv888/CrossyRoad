e85b358e546db0ec33f480cf58a5be6d4ef04d29
Bagrat Arutyunov <arutyunovbagrat@gmail.com>
2024-02-19 14:48:16 +0300

    add turtle

---
+++ main.py
@@ -0,0 +1,20 @@
+import time
+from turtle import Screen
+from player import Player
+
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.tracer(0)
+
+player = Player()
+
+game_is_on = True
+
+screen.listen()
+screen.onkeypress(player.go_up, "Up")
+screen.onkeypress(player.go_down, "Down")
+
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
